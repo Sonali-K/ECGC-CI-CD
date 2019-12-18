ecb1cb140db434924b4be04d8d3a6fcf1c1853e3
Sonali Kanse <sonali123@cdac.in>
2019-12-18 15:41:13 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EcgcComponent } from './ecgc/ecgc.component';
+import { SearchDeleteComponent } from './search-delete/search-delete.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:"",redirectTo:"enterdata",pathMatch:"full"},
+{path:"enterdata",component:EcgcComponent},
+{path:"searchdelete",component:SearchDeleteComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { EcgcComponent } from './ecgc/ecgc.component';
+import { SearchDeleteComponent } from './search-delete/search-delete.component';
+import { UserEcgcService } from './user-ecgc.service';
+import {HttpClientModule} from '@angular/common/http';
+import{ FormsModule } from  '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EcgcComponent,
+    SearchDeleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [UserEcgcService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
